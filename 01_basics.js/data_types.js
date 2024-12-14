b1c36e8afcb52679b61bd30a5ee370977ad55d5e
Malaya21 <138843670+Malaya21@users.noreply.github.com>
2024-12-14 14:23:43 +0000

    data types

---
+++ 01_basics.js/data_types.js
@@ -0,0 +1,14 @@
+// const first = undefined;
+// const second = null;
+// const third = Number;
+// const forth = String;
+// const fifth = Object;
+// sixth = structuredClone;
+// seven = type OfflineAudioCompletionEvent;
+// eght = BigInt;
+
+
+///Simply this all are data type avalable in js
+// some error are present
+
+
