0e360a725e13d7af94e2af9df54fdc96d6af0e04
Malaya21 <138843670+Malaya21@users.noreply.github.com>
2024-12-14 13:20:59 +0000

    console variables

--- 01_basics.js/Variables.js
+++ 01_basics.js/Variables.js
@@ -1,3 +1,9 @@
 let fullName = "Malaya Sahoo";
 const collegeName ="Ajaya Binaya Institute Of Technology";
 var schollName ="Gandhi Nodal Bidyapitha";
+console.log(fullName);
+console.log(collegeName);
+console.log(schollName);
+
+
+
