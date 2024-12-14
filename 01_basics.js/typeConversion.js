75347d25b13814473ca17a647355b488c33dd1a9
Malaya21 <138843670+Malaya21@users.noreply.github.com>
2024-12-14 14:47:06 +0000

    type conversion

---
+++ 01_basics.js/typeConversion.js
@@ -0,0 +1,20 @@
+// let i = -0;
+// let bool = Boolean(i);
+// console.log(typeof bool);
+// console.log( bool);
+
+//Value of zero treat as false and any other value treat as true
+
+// let str = "m";
+// bollStr = Boolean(str)
+// console.log(bollStr);
+
+//value of any empty string null and undefined is false
+
+
+// str = "10a";
+// num = Number(str);
+// console.log(typeof num);
+// console.log(num);
+// If converting number is not a number then it's type will changed to number but the value is NaN means Not a Number;
+
