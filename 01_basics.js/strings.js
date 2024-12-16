7cc34bdcb811b5f87256d1aa6d6bfdc81b90cbbd
Malaya21 <138843670+Malaya21@users.noreply.github.com>
2024-12-16 05:08:18 +0000

    String

---
+++ 01_basics.js/strings.js
@@ -0,0 +1,29 @@
+const string1 = "Malaya";
+const string3 = new String("Malaya");
+// console.log(string1)
+// console.log(typeof string1);
+// console.log(string3);
+// console.log(typeof string3);
+
+// console.log(string3[0]);
+// console.log(string3[1]);
+// console.log(string3[2]);
+// console.log(string3[3]);
+// console.log(string3[4]);
+
+// console.log(string1.charAt(1));
+// console.log(string3.charAt(1));
+// console.log(string3.indexOf("l"));
+
+// console.log(string1.toUpperCase());
+// console.log(string1.toLowerCase());
+
+// const newStr = string1.replace("Malaya","Tanmaya");
+// console.log(newStr);
+// console.log(string3.slice(0,6));
+console.log(string1.at(-6));
+
+
+
+
+
