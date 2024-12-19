a82619633a0c899ff11a31ad231b7d90ecb2c26f
Malaya21 <138843670+Malaya21@users.noreply.github.com>
2024-12-19 05:19:17 +0000

    some updates

--- 01_basics.js/strings.js
+++ 01_basics.js/strings.js
@@ -18,15 +18,15 @@ const string3 = new String("malaya");
 // console.log(string1.toUpperCase());
 // console.log(string1.toLowerCase());
 
-// const newStr = string1.replace("Malaya","Tanmaya");
+// const newStr = string1.replace("Malaya","Tanmaya");//It replace the charecters and returns the new string but it can not modify the original string
 // console.log(newStr);
-console.log(string1.slice(-5,-1));
+console.log(string1.slice(-5,-1));// it slice both +ve and negetive index and return new value
 // console.log(string1.at(2));
 // const join = string1.concat(" ",string3);
 // console.log(join);
-// console.log(string1.split('a'));
+// console.log(string1.split('a')); //It returns an array of spliting elements
 
-// console.log(string1.substring(-1,-3));
+// console.log(string1.substring(1,4));
 // console.log(string1.slice(-1));
 
 // console.log(string3.toWellFormed());
