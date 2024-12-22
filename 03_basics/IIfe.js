d0510e999ea9c11f7f3ce03086604630525d8b63
Malaya99 <sahomalaya21@gmail.com>
2024-12-22 17:43:19 +0530

    This is an IIFE function

---
+++ 03_basics/IIfe.js
@@ -0,0 +1,7 @@
+//This is a simple example of an IIFE (Immediately Invoked Function Expression)
+
+(function(){
+    console.log("This is an IIFE function");
+    
+})();
+//This is call automatically after writting this 
