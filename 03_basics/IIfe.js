201ca25f4335982e083d951c6a8802c776d18cc5
Malaya99 <sahomalaya21@gmail.com>
2024-12-23 21:07:23 +0530

    IFFI

--- 03_basics/IIfe.js
+++ 03_basics/IIfe.js
@@ -1,7 +1,26 @@
 //This is a simple example of an IIFE (Immediately Invoked Function Expression)
 
+
 (function(){
-    console.log("This is an IIFE function");
-    
-})();
-//This is call automatically after writting this 
+    console.log("This an IIFE function");
+    })();
+    //It must end with semicolon to avoid errors.
+    //It avoids polluting the global scope.
+    //It is a function that is executed immediately after it is created.
+    //It is also known as a self-invoking function.
+    //It is used to create a private scope.
+    //It is used to avoid naming conflicts.
+    (()=>{
+        console.log("This is an arrow function with IIFE function");
+
+        })();
+    //This is an self invoking function with arrow function.
+   
+    //It can also take arguments and return values.
+     const ans = (function(a,b){
+        console.log(a+b);
+        return a+b;      
+        })(2,3);
+       console.log(ans);
+       //It will return 5.
+        
